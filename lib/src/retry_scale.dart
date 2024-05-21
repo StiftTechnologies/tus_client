529d1003105affa8e90e93c9ce1d3e2e1d5b2933
EvertonMJunior <51867881+EvertonMJunior@users.noreply.github.com>
2024-05-21 15:59:50 -0300

    fix: fix typo on retry scale

--- lib/src/retry_scale.dart
+++ lib/src/retry_scale.dart
@@ -7,7 +7,7 @@ enum RetryScale {
   /// If interval is n, on every retry the the interval is increased by n.
   /// For example if [retryInterval] is set to 2 seconds, and the [retries] is set to 4,
   /// the interval for every retry is going to be [2, 4, 6, 8]
-  lineal,
+  linear,
 
   /// If interval is n, on every retry the last interval is going to be duplicated.
   /// For example if [retryInterval] is set to 2 seconds, and the [retries] is set to 4,
@@ -20,7 +20,7 @@ enum RetryScale {
     switch (this) {
       case RetryScale.constant:
         return Duration(seconds: retryInterval);
-      case RetryScale.lineal:
+      case RetryScale.linear:
         return Duration(seconds: (retry + 1) * retryInterval);
       case RetryScale.exponential:
         return Duration(seconds: retryInterval * pow(2, retry).toInt());
